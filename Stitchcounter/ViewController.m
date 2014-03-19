e3f9b2b7aecc650a07081c8f15878fc7882d2972
Jennie Louie <jennie.louie@gmail.com>
2014-03-19 16:07:19 -0700

    Initial Commit

---
+++ Stitchcounter/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Stitchcounter
+//
+//  Created by Jennifer Louie on 3/19/14.
+//  Copyright (c) 2014 Jennifer Louie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ StitchcounterTests/StitchcounterTests.m
@@ -0,0 +1,34 @@
+//
+//  StitchcounterTests.m
+//  StitchcounterTests
+//
+//  Created by Jennifer Louie on 3/19/14.
+//  Copyright (c) 2014 Jennifer Louie. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StitchcounterTests : XCTestCase
+
+@end
+
+@implementation StitchcounterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
