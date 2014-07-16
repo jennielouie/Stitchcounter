8e766e9040f15c85bb402507c4fff014c0266317
Jennie Louie <jennie.louie@gmail.com>
2014-07-15 22:31:11 -0700

    added table view controller

---
+++ Stitchcounter/SCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCTableViewController.h
+//  Stitchcounter
+//
+//  Created by Jennifer Louie on 7/15/14.
+//  Copyright (c) 2014 Jennifer Louie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCTableViewController : UITableViewController
+
+@end
